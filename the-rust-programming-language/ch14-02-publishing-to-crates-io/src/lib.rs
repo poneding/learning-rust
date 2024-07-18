7b66b59503cd4e34c2611893318a02896583bbd4
poneding <poneding@gmail.com>
2024-07-18 23:29:04 +0800

    .

--- the-rust-programming-language/ch14-02-publishing-to-crates-io/src/lib.rs
+++ the-rust-programming-language/ch14-02-publishing-to-crates-io/src/lib.rs
@@ -60,7 +60,7 @@ pub mod utils {
 
     /// Combines two primary colors in equal amounts to create
     /// a secondary color.
-    pub fn mix(c1: PrimaryColor, c2: PrimaryColor) -> SecondaryColor {
+    pub fn mix(_c1: PrimaryColor, _c2: PrimaryColor) -> SecondaryColor {
         SecondaryColor::Orange
         // --snip--
     }

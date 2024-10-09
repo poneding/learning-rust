51efacdcb116bdb222cb9f78016db1d6bb1de916
poneding <poneding@gmail.com>
2024-10-09 22:49:40 +0800

    .

--- programming-rust-book/ch02-07-quickreplace/src/main.rs
+++ programming-rust-book/ch02-07-quickreplace/src/main.rs
@@ -1,11 +1,47 @@
-use std::env::{self};
+use std::{
+    env::{self},
+    fs,
+};
 
 use text_colorizer::Colorize;
 
 // cargo run World Rust hello hello.out
 fn main() {
     let args = parse_args();
-    println!("{:?}", args);
+
+    let data = match fs::read_to_string(&args.filename) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!(
+                "{} failed to read from file '{}': {:?}",
+                "Error:".red().bold(),
+                args.filename,
+                e
+            );
+            std::process::exit(1);
+        }
+    };
+
+    let replaced_data = match regex_replace(&args.target, &args.replacement, &data) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("{} failed to replace text: {:?}", "Error:".red().bold(), e);
+            std::process::exit(1);
+        }
+    };
+
+    match fs::write(&args.output, &replaced_data) {
+        Ok(_) => {}
+        Err(e) => {
+            eprintln!(
+                "{} failed to write to file '{}': {:?}",
+                "Error:".red().bold(),
+                args.output,
+                e
+            );
+            std::process::exit(1);
+        }
+    }
 }
 
 #[derive(Debug)] //{:?}
@@ -45,4 +81,15 @@ fn parse_args() -> Arguments {
         filename: args[2].clone(),
         output: args[3].clone(),
     }
+}
+
+fn regex_replace(target: &str, replacement: &str, text: &str) -> Result<String, regex::Error> {
+    // match regex::Regex::new(target) {
+    //     Ok(rgx) => Ok(rgx.replace_all(text, replacement).to_string()),
+    //     Err(e) => Err(e),
+    // }
+
+    // 等效于
+    let rgx = regex::Regex::new(target)?;
+    Ok(rgx.replace_all(text, replacement).to_string())
 }
